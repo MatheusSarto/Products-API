524b6a77303f886da01b8d2043eb7635118dfed7
Matheus Sarto <matheusmsarto@gmail.com>
2024-01-30 08:45:00 -0300

    Initial commit

---
+++ demo-products-api/src/main/java/com/sartop/demoproductsapi/DemoProductsApiApplication.java
@@ -0,0 +1,13 @@
+package com.sartop.demoproductsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProductsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProductsApiApplication.class, args);
+	}
+
+}
